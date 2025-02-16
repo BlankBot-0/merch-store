d2804a780d7dac56452a23515b3bdbb05aee441e
vinilco <vinilco2@gmail.com>
2025-02-16 20:05:28 +0300

    refactor

--- internal/auth/errors.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import "errors"
-
-var ErrUnauthorized = errors.New("unauthorized")
-var ErrInvalidToken = errors.New("invalid token")
-var ErrForbidden = errors.New("forbidden")
--- internal/usecase/shop/errors.go
+++
@@ -1,8 +0,0 @@
-package shop
-
-import "errors"
-
-var (
-	ErrNotEnoughCoins  = errors.New("not enough coins")
-	ErrIncorrectAmount = errors.New("incorrect amount of coins")
-)
